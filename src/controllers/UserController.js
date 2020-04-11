8fb493564bea1c2fdafd3283d196b4070b6badf1
Iuri de Lima Marques <iurilmarques@hotmail.com>
2020-04-10 22:01:11 -0300

    routes configuration and configuration of error messages

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,11 +5,16 @@ module.exports = {
     async createUser(req, res) {
         let { nome, email, senha } = req.body;
 
+        if (!nome || !email || !senha) {
+            res.status(422).json({response: 'Necessario o preenchimento de todos os dados'});
+        }
+
         const sql = `SELECT * FROM usuarios WHERE email='${email}'`;
 
         connection.query(sql, function (error, results){
             if(error) {
-                return console.log(error);
+                return res.status(403).json({response: 'Ocorreu um erro desconhecido'}); 
+                console.log(error);
             } else{
                 if (results!='') {
                     res.json({response: 'usuario_existente'});
@@ -21,7 +26,8 @@ module.exports = {
         
                     connection.query(sql, function (error, results){
                         if(error) {
-                            return console.log(error);
+                            return res.status(403).json({response: 'Ocorreu um erro desconhecido'}); 
+                            console.log(error);
                         } else{
                             res.json({response: 'usuario_salvo'});
                         }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+//const cors = require('cors');
 const routes = require('./routes.js');
 
 const app = express();
 
+/*const whitelist = ['http://localhost:3000/api-ecommerce'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}*/
+
+//app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(routes);
+app.use('/api-ecommerce', routes);
 
 app.listen(3000);
